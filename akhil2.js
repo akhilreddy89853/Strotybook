00ffc2205d93f2168f5636101c94f524d19b687c
Akhil Reddy <some1234@gmail.com>
2019-03-12 11:35:22 +0530

    new server

---
+++ akhil2.js
@@ -0,0 +1,33 @@
+
+var http=require("http");
+const express = require('express');
+const path = require('path');
+var app = require('express')();
+app.use("/", express.static(path.join(__dirname, "./public")));
+
+app.get('/Story/photo/:id',function(req,res){
+	var number = req.params.id;
+	console.log(number);
+	
+   if(number == 1){
+       const fs = require('fs');
+       var  config = require('./story.json');
+    //res.send('<html><h1>' +config.title_en+ '</h1> <img src ='+'/'+config.cover_image+' width ="340" height="250"> </html>')
+	res.send('<html lang="te" dir="ltr"><head><meta charset="utf-8"><title>Story book</title><style>img {margin: auto;display: block;}.ali {text-align: center;} h3 {text-align:center}button {float: right;background-color: burlywood;font-size: 30;}</style> </head><body><h3>' +config.title_en+ '</h3><br><img  src = '+'/'+config.cover_image+' height=300px; width="300px"><br><h3>'+ config.title+'</h3><button><a href="'+(number - 1 + 2)+'">Nextpage</a></button></body></html>');
+	}
+	 else if(number >= 2){
+		 const fs = require('fs');
+       let data = fs.readFileSync('story.json', (err) => {if (err) throw err;});
+       let data1 = JSON.parse(data);
+       var pages = data1.pages;
+       var currentPage = pages[number -2];
+       if(pages[number - 2] == null){
+           res.send('<html lang="te" dir="ltr"><head><meta charset="utf-8">  <style> *{ margin: 0px; padding: 0px;}body{width: 550px;height: 400px;}.n1{ position: relative;top: 250px;left: 600px;}</style></head><body><div class="n1"><h1>THE END</h2></div></body></html>');
+        }
+       console.log(pages[number - 2]); 
+	  res.send('<html lang="te" dir="ltr"><head><meta charset="utf-8"><title>Story book</title><style>img {margin: auto;display: block;}.ali {text-align: center;} h3{text-align:center}</style> </head><body><h3>'+currentPage.english+'</h3><br><img  src = '+'/'+currentPage.image+' height=300px; width="300px"><br><h3>'+ currentPage.telugu+'</h3><button float:right><a href="'+(number - 1)+'">Previous</a></button><button float:left><a href="'+(number - 1 + 2)+'">Nextpage</a></button></body></html>');
+}
+});
+app.listen(3000, function() {;
+console.log("Server start");
+});
